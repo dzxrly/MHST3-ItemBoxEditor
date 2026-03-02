123804a9565821953e752cb8260bdaf9c79ebca5
dzxrly2 <dzxrly2@gmail.com>
2026-03-02 18:40:53 +0800

    first commit

---
+++ src/ItemBoxEditor/config.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+M.LARGE_BTN = Vector2f.new(300, 50)
+M.SMALL_BTN = Vector2f.new(200, 40)
+M.ERROR_COLOR = 0xeb4034ff
+M.CHECKED_COLOR = 0xff74ff33
+M.VARY_BTN = Vector2f.new(140, 30)
+M.VARY_GROUP_WIDTH = 300
+M.MAX_NUM = 999
+M.ALLOWED_CATEGORIES_CONTENT = {
+    [0] = "IT_MC_BATTLE",
+    [1] = "IT_MC_MATERIAL",
+    [2] = "IT_MC_FACILITY",
+    [3] = "IT_MC_GROWTH",
+    [4] = "IT_MC_VALUABLES"
+}
+
+return M
---
+++ src/item_box_editor.lua
@@ -0,0 +1,48 @@
+local coreApi = require("ItemBoxEditor.utils")
+local state = require("ItemBoxEditor.state")
+local init = require("ItemBoxEditor.init")
+local i18n = require("ItemBoxEditor.i18n")
+local config = require("ItemBoxEditor.config")
+local ui = require("ItemBoxEditor.ui")
+
+coreApi.init("ItemBoxEditor")
+
+local isBtnClicked = false
+
+-- DO NOT CHANGE THE NEXT LINE, ONLY UPDATE THE VERSION NUMBER
+local modVersion = "v0.1.0"
+-- DO NOT CHANGE THE PREVIOUS LINE
+
+sdk.hook(sdk.find_type_definition("app.SaveDataManager"):get_method("getTitleText()"), function(args)
+end, function(retval)
+    state.resetState()
+    init.onStart()
+    coreApi.setUserCmdPostHook(init.modInit)
+    return retval
+end)
+
+re.on_draw_ui(function()
+    if imgui.tree_node("Item Box Editor") then
+        imgui.text("VERSION: " .. modVersion .. " | by Egg Targaryen")
+        imgui.text_colored(i18n.getUIText("save_data_warning"), config.ERROR_COLOR)
+        imgui.text_colored(i18n.getUIText("save_data_warning"), config.ERROR_COLOR)
+        imgui.text_colored(i18n.getUIText("save_data_warning"), config.ERROR_COLOR)
+        imgui.new_line()
+
+        if imgui.button(i18n.getUIText("read_item_box_btn"), config.LARGE_BTN) then
+            coreApi.executeUserCmd(function()
+                coreApi.log("Read item box btn clicked")
+                state.resetState()
+                init.onStart()
+                isBtnClicked = true
+            end)
+        end
+
+        imgui.new_line()
+        if state.cUserSaveDataParam ~= nil and isBtnClicked and #state.comboItemOptions.displayText > 0 then
+            ui.drawUI()
+        end
+
+        imgui.tree_pop()
+    end
+end)
